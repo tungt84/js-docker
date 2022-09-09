aa10b5657d60626c9cb0f38f30ce71702a1fb2bf
U-SACOMBANK\tungt <tungt@LT-BNKBG003.sacombank.corp.vn>
2022-09-09 15:54:03 +0700

    add ce docker

--- jaspersoft-containers/Docker/jrs/scripts/entrypoint.sh
+++ jaspersoft-containers/Docker/jrs/scripts/entrypoint.sh
@@ -6,5 +6,5 @@
 
 set -e
 #echo 'net.sf.jasperreports.chrome.argument.no-sandbox=true' >>$CATALINA_HOME/webapps/jasperserver-pro/WEB-INF/classes/jasperreports.properties
-#echo 'net.sf.jasperreports.chrome.argument.disable-dev-shm-usage=true' >>$CATALINA_HOME/webapps/jasperserver-pro/WEB-INF/classes/jasperreports.properties
+echo 'net.sf.jasperreports.chrome.argument.disable-dev-shm-usage=true' >>$CATALINA_HOME/webapps/jasperserver/WEB-INF/classes/jasperreports.properties
 exec "$@"
